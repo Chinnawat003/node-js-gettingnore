fc60c17a4331a1f00712d6ff74cae0b9233ca65b
Chinnawat2003 <chinnawat1926@gmail.com>
2022-07-11 15:51:43 +0700

    Up1

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const msg = require('./message.js')
+
+const host = "localhost"
+const PORT = 5000
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-Type":"text/pain"})
+    res.write(`${msg.message} I like a cat`)
+    res.end()
+}).listen(PORT, ()=>{
+    console.log('Start Sever runing at http//:${host}:{PORT}')
+})
