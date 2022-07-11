8e2e8933540bac395185ddc31fafb922e7c5b1d1
Chinnawat2003 <chinnawat1926@gmail.com>
2022-07-11 16:07:10 +0700

    Up2

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require('http')
 const msg = require('./message.js')
 
 const host = "localhost"
-const PORT = 5000
+const PORT = process.env.PORT || 5000
+
 const server = http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type":"text/pain"})
     res.write(`${msg.message} I like a cat`)
